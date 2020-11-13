f6f2c4a7580a34eaf3b507073ae7f72b85250a29
irisida <20445648+irisida@users.noreply.github.com>
2020-11-13 19:13:09 +0000

    unrelated vaiables and sructs

--- core/src/10-structs/1001-create-structs/main.go
+++ core/src/10-structs/1001-create-structs/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+type book struct {
+	title  string
+	author string
+	year   int
+}
+
+func main() {
+	// collection of unrelated variables example
+	title1, author1, year1 := "The Devine Comedy", "Dante Aligheri", 1320
+	title2, author2, year2 := "MacBeth", "William Shakespear", 1606
+
+	fmt.Println("book1: ", title1, author1, year1)
+	fmt.Println("book2: ", title2, author2, year2)
+
+	// examples of structured data
+	book1 := book{title: "The Devine Comedy", author: "Dante Aligheri", year: 1320}
+	book2 := book{title: "MacBeth", author: "William Shakespear", year: 1606}
+
+	// print representation usual the struct we created instead of multiple variables
+	fmt.Println(book1)
+	fmt.Println(book2)
 }
