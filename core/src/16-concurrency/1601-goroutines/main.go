fdb77d80379527327fda9db600edce44e2e3c1e5
irisida <20445648+irisida@users.noreply.github.com>
2020-11-21 17:02:58 +0000

    concurrency typical functions for checking system

--- core/src/16-concurrency/1601-goroutines/main.go
+++ core/src/16-concurrency/1601-goroutines/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+)
+
 func main() {
 
+	fmt.Println("Main execution started...")
+	fmt.Println("No of CPUs : ", runtime.NumCPU())
+	fmt.Println("No of goroutines: ", runtime.NumGoroutine())
+
+	fmt.Println("OS: ", runtime.GOOS)
+	fmt.Println("ARCH: ", runtime.GOARCH)
+
+	fmt.Println("Max Procs", runtime.GOMAXPROCS(0))
 }
